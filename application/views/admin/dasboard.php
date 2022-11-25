ed4ef7497c28eb65515352961e6eda78f3c669ff
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2022-11-25 14:01:23 +0700

    edit tampilan home

--- application/views/admin/dasboard.php
+++ application/views/admin/dasboard.php
@@ -24,16 +24,20 @@
     </div>
     <div class="row text-center mt-4">
         <?php foreach ($produk as $pdk) : ?>
-            <div class="card ml-3 mb-3" style="width: 16rem;">
-                <img src="<?= base_url() . '/upload/' . $pdk->gambar ?>" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $pdk->nama_brg ?></h5>
-                    <small><?= $pdk->keterangan ?></small><br>
-                    <span class="badge badge-info mb-3">Rp. <?= number_format($pdk->harga, 0, ',', '.')  ?></span>
-                    <?= anchor('dasboard/tambah_keranjang/' . $pdk->id_brg, '<div class="btn btn-primary btn-sm">Tambah Ke keranjang</div>') ?>
-                    <?= anchor('dasboard/detail_produk/' . $pdk->id_brg, '<div class="btn btn-success btn-sm">Detail</div>') ?>
-                    <!-- <a href="#" class="btn btn-sm btn-primary">Tambar Ke Keranjang</a> -->
-                    <!-- <a href="#" class="btn btn-sm btn-success">Detail</a> -->
+            <div class="col-lg-4 col-md-6 col-xl-3">
+                <div class="card ml-3 mb-3 ">
+                    <div class="card-body p-0">
+                        <img src="<?= base_url() . '/upload/' . $pdk->gambar ?>" class="card-img-top" alt="..." style="height: 300px; object-fit: cover;    object-position: center; border-radius: 8px;">
+                        <div class="p-4">
+                            <h5 class="card-title"><?= $pdk->nama_brg ?></h5>
+                            <small><?= $pdk->keterangan ?></small><br>
+                            <span class="badge badge-info mb-3">Rp. <?= number_format($pdk->harga, 0, ',', '.')  ?></span><br>
+                            <?= anchor('dasboard/tambah_keranjang/' . $pdk->id_brg, '<div class="btn btn-primary btn-sm">Tambah Ke keranjang</div>') ?>
+                            <?= anchor('dasboard/detail_produk/' . $pdk->id_brg, '<div class="btn btn-success btn-sm">Detail</div>') ?>
+                            <!-- <a href="#" class="btn btn-sm btn-primary">Tambar Ke Keranjang</a> -->
+                            <!-- <a href="#" class="btn btn-sm btn-success">Detail</a> -->
+                        </div>
+                    </div>
                 </div>
             </div>
         <?php endforeach; ?>
